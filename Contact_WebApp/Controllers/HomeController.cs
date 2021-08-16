b9969810f6d4bc2fa8a73a0d87ae94d509d69436
Daniella Noren <daniellanoren@live.se>
2021-08-16 14:41:24 +0200

    Add razor-pages, routing, controller, layout

---
+++ Contact_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult About()
+        {
+            return View();
+        } 
+
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
--- Contact_WebApp/Startup.cs
+++ Contact_WebApp/Startup.cs
@@ -1,3 +1,4 @@
+using Contact_WebApp.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,24 +17,24 @@ namespace Contact_WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
-
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "home", "{controller=Home}/{action=About}");
             });
         }
     }
