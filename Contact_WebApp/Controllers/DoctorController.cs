ea29e707b5a60ea1c959a7ae3111a2a5a89e8889
Daniella Noren <daniellanoren@live.se>
2021-08-17 16:09:02 +0200

    Add temperature-util, return Illnessresult-model and print out on screen

--- Contact_WebApp/Controllers/DoctorController.cs
+++ Contact_WebApp/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Contact_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,16 @@ namespace Contact_WebApp.Controllers
     
     public class DoctorController : Controller
     {
-        [Route("FeverCheck", Name = "fevercheck")]
+        [HttpGet("FeverCheck", Name = "fevercheck")]
         public IActionResult FeverCheck()
         {
             return View();
+        }
+
+        [HttpPost("FeverCheck", Name = "fevercheck")]
+        public IActionResult FeverCheck(long temp)
+        {
+            return View(new IllnessResult(temp));
         }
     }
 }
---
+++ Contact_WebApp/Models/TemperatureUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact_WebApp.Models
+{
+    public class TemperatureUtil
+    {
+        private static readonly long FeverStart = 38;
+        private static readonly long HypothermiaEnd = 35;
+
+        public static bool HasFever(long temp)
+        {
+            return temp >= FeverStart;
+        }
+
+        public static bool HasHypothermia(long temp)
+        {
+            return temp < HypothermiaEnd;
+        }
+
+    }
+
+    public class IllnessResult
+    {
+        public bool HasFever { get; set; }
+        public bool HasHypothermia { get; set; }
+
+        public IllnessResult(long temp)
+        {
+            this.HasFever = TemperatureUtil.HasFever(temp);
+            this.HasHypothermia = TemperatureUtil.HasHypothermia(temp);
+        }
+    }
+}
