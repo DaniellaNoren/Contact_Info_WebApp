de87d416d462a69ccee811d9f5ad9f6e516865d9
Daniella Noren <daniellanoren@live.se>
2021-08-17 12:54:30 +0200

    Add new controller. custom route and view. Change css to fit new route in menu

---
+++ Contact_WebApp/Controllers/DoctorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact_WebApp.Controllers
+{
+    
+    public class DoctorController : Controller
+    {
+        [Route("FeverCheck", Name = "fevercheck")]
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+    }
+}
